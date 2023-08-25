f8e0d5a172985c3f4a760928e898d971917cd46f
HoangHuuToan <109668328+HoangHuuToan@users.noreply.github.com>
2023-08-25 15:26:10 +0700

    update

---
+++ Tool_Comment/Tool_Comment/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace Tool_Comment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+        string folderPathRoot = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult dialogResult = folderBrowserDialog.ShowDialog();
+            if (dialogResult == System.Windows.Forms.DialogResult.OK) 
+            {
+                folderPathRoot = folderBrowserDialog.SelectedPath;
+
+                
+
+            }
+
+        }
+    }
+}
